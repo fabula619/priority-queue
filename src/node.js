ff00bb0329c7b7e406c095a7d368ff089e4b26ae
fabula619 <fabula619@gmail.com>
2017-11-13 23:02:15 +0300

    firsr commit

--- src/node.js
+++ src/node.js
@@ -1,23 +1,94 @@
 class Node {
-	constructor(data, priority) {
+    constructor(data, priority) {
+        this.data = data;
+        this.priority = priority;
+        this.left = null;
+        this.right = null;
+        this.parent = null;
+    }
 
-	}
+    appendChild(node) {
+        if (this.right == null && this.left != null) {
+            this.right = node;
+            this.right.parent = this;
+        }
+        if (this.left == null) {
+            this.left = node;
+            this.left.parent = this;
+        }
+    }
 
-	appendChild(node) {
+    removeChild(node) {
+        if (this.left == node) {
+            this.left.parent = null;
+            this.left = null;
+        }
+        else if (this.right == node) {
+            this.right.parent = null;
+            this.right = null;
+        }
+        else
+            throw err;
+    }
 
-	}
+    remove() {
+        if (this.parent != null)
+            this.parent.removeChild(this);
+        else {
+            this.obj = null;
+        }
+    }
 
-	removeChild(node) {
+    swapWithParent() {
+        if(this.parent != null) {
 
-	}
+            let flag = -1;
+            if(this.parent.parent !=null) {
+                if (this.parent.parent.left == this.parent)
+                    flag = 1;
+                if (this.parent.parent.right == this.parent)
+                    flag = 0;
+            }
+            var for_right = this.parent.right;
+            var for_left = this.parent.left;
+            var chi_left = this.left;
+            var chi_right = this.right;
+            var our_child = this;
+            var parent = this.parent;
 
-	remove() {
+            var temp_reference = parent.parent;
+            let left_flag=false;
+            if(this == this.parent.left)
+                left_flag = true;
+            parent.parent = this;
+            our_child.parent = temp_reference;
+            if(left_flag) {
+                our_child.left = parent;
+                if(for_right != null) {
+                    our_child.right = for_right;
+                    our_child.right.parent = our_child;
+                }
+            }
+            else
+            {
+                our_child.right = parent;
+                if( for_left !=null) {
+                    our_child.left = for_left;
+                    our_child.left.parent = our_child;
+                }
+            }
+            parent.left = chi_left
+            parent.right = chi_right;
+            if(chi_left!=null)
+                parent.left.parent =  parent;
+            if(chi_right!=null)
+                parent.right.parent = parent;
+            if(flag ==1 )
+                our_child.parent.left = our_child;
+            else if(flag ==0)
+                our_child.parent.right = our_child;
+        }
 
-	}
-
-	swapWithParent() {
-		
-	}
+    }
 }
-
 module.exports = Node;
--- src/queue.js
+++ src/queue.js
@@ -1,25 +1,55 @@
+
+
+
 const MaxHeap = require('./max-heap.js');
 
 class PriorityQueue {
 	constructor(maxSize) {
-
+		this.heap = new MaxHeap();
+        this.maxSize = maxSize ? maxSize : 30;
 	}
 
 	push(data, priority) {
-
+		if(this.heap.heapSize >= this.maxSize)
+			throw err;
+		this.heap.push(data,priority);
 	}
 
 	shift() {
+		if(this.size() <=0 )
+			throw err;
+       return this.heap.pop();
 
 	}
 
 	size() {
-
+		return this.heap.size();
 	}
 
 	isEmpty() {
-		
-	}
+        return this.size() == 0;
+    }
 }
 
+
+let q = new PriorityQueue();
+const nodes = [
+    { priority: 10, data: 1 },
+    { priority: 20, data: 2 },
+    { priority:  5, data: 3 },
+    { priority:  0, data: 4 },
+    { priority:  8, data: 5 },
+    { priority: 12, data: 6 },
+    { priority: 17, data: 7 },
+    { priority: 15, data: 8 },
+];
+const expectedData = [2, 7, 8, 6, 1, 5, 3, 4];
+nodes.forEach(node => q.push(node.data, node.priority));
+let t= 234;
+t++;
+
 module.exports = PriorityQueue;
+
+
+
+
